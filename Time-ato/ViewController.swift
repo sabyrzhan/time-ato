b193aea08c5c1c2f3d0a07bd2a9a00aff6b5ddee
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-09-24 16:56:40 +0300

    Initial Commit

---
+++ Time-ato/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Time-ato
+//
+//  Created by Sabyrzhan Tynybayev on 24.09.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
